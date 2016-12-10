2a4fa685521725283f34dfc7b369bd6143db6fad
Peter Withers <peter-gthb@bambooradical.com>
2016-12-10 19:33:01 +0100

    Added interrupt routine to read PPM input from the RX, however only pins 2 and 3 can handle interrupts.

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,31 @@
 NewPing sonarForward(TRIGGER_PIN_FORWARD, ECHO_PIN_FORWARD, MAX_DISTANCE);
 //NewPing sonarRight(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
 
+volatile unsigned long throttlePulseLastChangeMs = 0;
+volatile int pulseWidthThrottle = 0;
+#define THROTTLE_PIN A0
+
+void throttleSignalInterrupt() {
+    if ((digitalRead(THROTTLE_PIN) == 0)) {
+        pulseWidthThrottle = millis() - throttlePulseLastChangeMs;
+    } else {
+        throttlePulseLastChangeMs = millis();
+    }
+}
+
 void setup() {
     Serial.begin(57600);
     delay(10);
     digitalWrite(13, 1);
+    pinMode(THROTTLE_PIN, INPUT);
+    //    digitalWrite(THROTTLE_PIN, HIGH); // the receiver should not need the pull up enabled
+    attachInterrupt(digitalPinToInterrupt(THROTTLE_PIN), throttleSignalInterrupt, CHANGE);
 }
 
 void loop() {
     delay(1000);
     int forwardDistance = sonarForward.ping_cm();
-//    int rightDistance = sonarRight.ping_cm(); // @todo
+    //    int rightDistance = sonarRight.ping_cm(); // @todo
     Serial.print("forward: ");
     Serial.print(forwardDistance);
     Serial.print("cm right:");
@@ -32,8 +47,12 @@ void loop() {
     Serial.print("cm in ");
     Serial.print(millis());
     Serial.println("ms");
-    if (forwardDistance > 100)
+    if (forwardDistance > 100) {
         digitalWrite(13, 1);
-    else
+    } else {
         digitalWrite(13, 0);
+    }
+    Serial.print("Throttle: ");
+    Serial.print(pulseWidthThrottle);
+    Serial.println("ms");
 }
