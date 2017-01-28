7051434c5296662aecbbd56c1c6c93e3abd7a49f
harmjanfl <harmjanfl@gmail.com>
2017-01-28 16:55:41 +0100

    added height sonar

--- src/height dependent trottle.cpp
+++
@@ -1,7 +0,0 @@
-#include <NewPing.h>
-
-#define TRIGGER_PIN_HEIGHT  3
-#define ECHO_PIN_HEIGHT     2
-#define MAX_DISTANCE 400
-
-NewPing sonarHeight(TRIGGER_PIN_HEIGHT, ECHO_PIN_HEIGHT, MAX_DISTANCE);
--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,26 @@
 
 #include <NewPing.h>
 
-#define TRIGGER_PIN_FORWARD  3
-#define ECHO_PIN_FORWARD     2
+#define TRIGGER_PIN_HEIGHT  3
+#define ECHO_PIN_HEIGHT     2
+//#define TRIGGER_PIN_FORWARD  3
+//#define ECHO_PIN_FORWARD     2
 
-#define TRIGGER_PIN_RIGHT
-#define ECHO_PIN_RIGHT
+//#define TRIGGER_PIN_RIGHT
+//#define ECHO_PIN_RIGHT
 // @todo: add defines for TRIGGER_PIN_RIGHT and ECHO_PIN_RIGHT once the desired pins have been assigned
 // @todo: add left and back sonar definitions
 #define MAX_DISTANCE 400
 
-NewPing sonarForward(TRIGGER_PIN_FORWARD, ECHO_PIN_FORWARD, MAX_DISTANCE);
-NewPing sonarRight(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
+NewPing sonarHeight(TRIGGER_PIN_HEIGHT, ECHO_PIN_HEIGHT, MAX_DISTANCE);
+
+//NewPing sonarForward(TRIGGER_PIN_FORWARD, ECHO_PIN_FORWARD, MAX_DISTANCE);
+//NewPing sonarRight(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
 
 volatile unsigned long throttlePulseLastChangeMs = 0;
 volatile int pulseWidthThrottle = 0;
 #define THROTTLE_PIN A0
-#define YAW_PIN A1
+//#define YAW_PIN A1
 
 ISR(PCINT1_vect) {
     // @ todo: interrupts will fire here for A0-A3, if you use more than one you will need to test which pin changed
@@ -45,20 +49,31 @@ void setup() {
 
 void loop() {
     delay(1000);
-    int forwardDistance = sonarForward.ping_cm();
+    //int forwardDistance = sonarForward.ping_cm();
+    int verticalDistance = sonarHeight.ping_cm();
     //    int rightDistance = sonarRight.ping_cm(); // @todo
-    Serial.print("forward: ");
-    Serial.print(forwardDistance);
-    Serial.print("cm right:");
+    //Serial.print("forward: ");
+    //Serial.print(forwardDistance);
+    Serial.print("vertical: ");
+    Serial.print(verticalDistance);
+
+    Serial.print("cm vertical:");
     Serial.print("(todo)"); // @ todo
     Serial.print("cm in ");
     Serial.print(millis());
     Serial.println("ms");
-    if (forwardDistance > 100) {
+    /*if (forwardDistance > 100) {
+        digitalWrite(13, 1);
+    } else {
+        digitalWrite(13, 0);
+    }*/
+
+    if (verticalDistance > 100) {
         digitalWrite(13, 1);
     } else {
         digitalWrite(13, 0);
     }
+    
     Serial.print("Throttle: ");
     Serial.print(pulseWidthThrottle);
     Serial.println("us");
